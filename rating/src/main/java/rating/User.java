9e3b6fc788a690b41211b8fd18f769b222eb30c6
Adithiyan-R <98727876+Adithiyan-R@users.noreply.github.com>
2022-03-10 16:28:01 +0530

    Add files via upload

---
+++ rating/src/main/java/rating/User.java
@@ -0,0 +1,46 @@
+package rating;
+
+public class User {
+	
+	private String username;
+	private String emailid;
+	private String password;
+	private String mobilenumber;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmailid() {
+		return emailid;
+	}
+
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobilenumber() {
+		return mobilenumber;
+	}
+
+	public void setMobilenumber(String mobilenumber) {
+		this.mobilenumber = mobilenumber;
+	}
+	
+}
